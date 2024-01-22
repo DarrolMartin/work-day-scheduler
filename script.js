6f938c1be31f2821ef2100891c8d8b6303d643b7
Darrol <Darrol>
2024-01-22 23:21:43 +0000

    added past ,present and future function as well as previous, description text area

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-    console.log("hello");
+console.log("hello");
     // Function to get the current date and display it
     function displayCurrentDate() {
       const currentDate = dayjs().format('dddd, MMMM D, YYYY');
       $('#currentDay').text(currentDate);
-      console.log(currentDate)
+console.log(currentDate)
       ;
     }
    // Function to create time blocks
@@ -25,6 +25,18 @@ console.log(timeBlock);
   // Create hour element
   const hourElement = $('<div>').addClass('col-md-1 hour');
   hourElement.text(dayjs().hour(hour).format('hA'));
+
+    // Create description textarea
+    const descriptionElement = $('<textarea>').addClass('col-md-10 description');
+
+     // Set past, present, or future class based on the current hour
+     if (hour < currentHour) {
+        descriptionElement.addClass('past');
+      } else if (hour === currentHour) {
+        descriptionElement.addClass('present');
+      } else {
+        descriptionElement.addClass('future');
+      }
 }
 }
 // Call the functions
