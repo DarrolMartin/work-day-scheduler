59722b43e5f31bd8f8b2f08affbdb51cfb2b7f1f
Darrol <Darrol>
2024-01-22 12:18:18 +0000

    added starter files

---
+++ script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() 
+// {
+//     console.log("hello") ;});
+
+    // $(document).ready(function() {
+    //     console.log("hello");
+    // });
+    
+
+
+    // Function to get the current date and display it
+    $(function() {
+        console.log("hello");
+    });
+    
