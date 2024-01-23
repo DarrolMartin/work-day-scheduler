ce8ac6ddbbd476201bada830a13fc8c0c1b3bfb6
Darrol <Darrol>
2024-01-23 03:01:15 +0000

    blocked google link with 404 error

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-console.log("hello");
+// console.log("hello");
     // Function to get the current date and display it
     function displayCurrentDate() {
       const currentDate = dayjs().format('dddd, MMMM D, YYYY');
       $('#currentDay').text(currentDate);
-console.log(currentDate)
+// console.log(currentDate)
       ;
     }
    // Function to create time blocks
@@ -13,15 +13,15 @@ console.log(currentDate)
     {
         const workdayStartHour = 9; // Assuming workday starts at 9 AM
         const workdayEndHour = 17; // Assuming workday ends at 5 PM
-console.log(workdayStartHour, workdayEndHour);
+// console.log(workdayStartHour, workdayEndHour);
         const currentHour = dayjs().hour();
-console.log(currentHour);
+// console.log(currentHour);
         // Get the schedule container
         const scheduleContainer = $('.container');
 
         for (let hour = workdayStartHour; hour <= workdayEndHour; hour++) {
             const timeBlock = $('<div>').addClass('row time-block');
-console.log(timeBlock);
+// console.log(timeBlock);
     // Create hour element
     const hourElement = $('<div>').addClass('col-md-1 hour');
     hourElement.text(dayjs().hour(hour).format('hA'));
@@ -37,20 +37,36 @@ console.log(timeBlock);
             } else {
                 descriptionElement.addClass('future');
             }
+            // Retrieve saved text from localStorage and set it in the description textarea
+      const savedText = localStorage.getItem(`hour-${hour}`);
+      if (savedText) {
+        descriptionElement.val(savedText);
+      }
             
-            
-            
-       // Create save button
-       const saveBtn = $('<button>').addClass('col-md-1 saveBtn').html('<i class="fas fa-save"></i>');  
- 
+                // Create save button
+      const saveBtn = $('<button>').addClass('col-md-1 saveBtn').html('<i class="fas fa-save"></i>');
 
-         
-        
-        
-        }}
+            // Save button click event
+            saveBtn.on('click', function() {
+              const textToSave = descriptionElement.val();
+              localStorage.setItem(`hour-${hour}`, textToSave);
+              alert('Text saved successfully!');
+            });
+      
+            // Append elements to time block
+            timeBlock.append(hourElement, descriptionElement, saveBtn);
+      
+            // Append time block to schedule container
+            scheduleContainer.append(timeBlock);
+          }
+        }
+      
+        // Display current date and create time blocks on page load
+        displayCurrentDate();
+        createTimeBlocks();
 
-// Call the functions
-displayCurrentDate();
-createTimeBlocks();
-    
-});
+
+        
+      });
+      
+            
