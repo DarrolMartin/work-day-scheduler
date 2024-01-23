81898a1f3668416a961dc902a6ff85bf2e3b1c1b
Darrol <Darrol>
2024-01-23 00:40:09 +0000

    Save button

--- script.js
+++ script.js
@@ -9,48 +9,48 @@ console.log(currentDate)
       ;
     }
    // Function to create time blocks
-  function createTimeBlocks() 
-  {
-    const workdayStartHour = 9; // Assuming workday starts at 9 AM
-    const workdayEndHour = 17; // Assuming workday ends at 5 PM
+    function createTimeBlocks() 
+    {
+        const workdayStartHour = 9; // Assuming workday starts at 9 AM
+        const workdayEndHour = 17; // Assuming workday ends at 5 PM
 console.log(workdayStartHour, workdayEndHour);
-    const currentHour = dayjs().hour();
+        const currentHour = dayjs().hour();
 console.log(currentHour);
-    // Get the schedule container
-    const scheduleContainer = $('.container');
+        // Get the schedule container
+        const scheduleContainer = $('.container');
 
-    for (let hour = workdayStartHour; hour <= workdayEndHour; hour++) {
-      const timeBlock = $('<div>').addClass('row time-block');
+        for (let hour = workdayStartHour; hour <= workdayEndHour; hour++) {
+            const timeBlock = $('<div>').addClass('row time-block');
 console.log(timeBlock);
-  // Create hour element
-  const hourElement = $('<div>').addClass('col-md-1 hour');
-  hourElement.text(dayjs().hour(hour).format('hA'));
+    // Create hour element
+    const hourElement = $('<div>').addClass('col-md-1 hour');
+    hourElement.text(dayjs().hour(hour).format('hA'));
 
-    // Create description textarea
-    const descriptionElement = $('<textarea>').addClass('col-md-10 description');
+        // Create description textarea
+        const descriptionElement = $('<textarea>').addClass('col-md-10 description');
 
-     // Set past, present, or future class based on the current hour
-     if (hour < currentHour) {
-        descriptionElement.addClass('past');
-      } else if (hour === currentHour) {
-        descriptionElement.addClass('present');
-      } else {
-        descriptionElement.addClass('future');
-      }
-      
-      
-      
-    }
+         // Set past, present, or future class based on the current hour
+         if (hour < currentHour) {
+                descriptionElement.addClass('past');
+            } else if (hour === currentHour) {
+                descriptionElement.addClass('present');
+            } else {
+                descriptionElement.addClass('future');
+            }
+            
+            
+            
+       // Create save button
+       const saveBtn = $('<button>').addClass('col-md-1 saveBtn').html('<i class="fas fa-save"></i>');  
  
 
-     
-    
-    // Append time block to schedule container
-      scheduleContainer.append(timeBlock);
-}
-}
+         
+        
+        
+        }}
+
 // Call the functions
 displayCurrentDate();
 createTimeBlocks();
+    
 });
-  
